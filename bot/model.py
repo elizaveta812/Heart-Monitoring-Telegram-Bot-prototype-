7a061645ef0c79d66d109f573f7dcef1af0be176
Елизавета <e.zimina@alumni.nsu.ru>
2025-06-06 16:16:28 +0700

    Добавила загрузку модели

--- bot/model.py
+++ bot/model.py
@@ -6,9 +6,15 @@ class ModelHandler:
         self.model = self.load_model(model_path)
 
     def load_model(self, model_path):
-        # Загрузка модели
-        return joblib.load(model_path)  # или keras.models.load_model(model_path)
+        # загрузка модели
+        return joblib.load(model_path)
 
     def predict(self, features):
-        # Предсказание
+        # предсказание
         return self.model.predict(features)
+
+
+# использование
+if __name__ == "__main__":
+    model_path = "models/mfdp_model.pkl"
+    model_handler = ModelHandler(model_path)
