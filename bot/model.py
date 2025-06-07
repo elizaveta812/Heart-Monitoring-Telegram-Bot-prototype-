12b39286462b21771f7d765a3105beeec6168180
Елизавета <e.zimina@alumni.nsu.ru>
2025-06-07 07:34:43 +0700

    Подправила bot/model.py

--- bot/model.py
+++ bot/model.py
@@ -5,16 +5,11 @@ class ModelHandler:
     def __init__(self, model_path):
         self.model = self.load_model(model_path)
 
-    def load_model(self, model_path):
+    @staticmethod
+    def load_model(model_path):
         # загрузка модели
         return joblib.load(model_path)
 
     def predict(self, features):
         # предсказание
         return self.model.predict(features)
-
-
-# использование
-if __name__ == "__main__":
-    model_path = "models/mfdp_model.pkl"
-    model_handler = ModelHandler(model_path)
