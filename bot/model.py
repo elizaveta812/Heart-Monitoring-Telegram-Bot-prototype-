c6dc9038bfd9e3dfda198cfb43ff136d7a1eed53
Елизавета <e.zimina@alumni.nsu.ru>
2025-06-17 15:46:29 +0700

    Исправления после фидбэка - добавила синглтон, чтобы загружать модель один раз и сделать доступной из всех частей проекта

--- bot/model.py
+++ bot/model.py
@@ -2,14 +2,19 @@ import joblib
 import os
 
 
-class ModelHandler:
-    def __init__(self, model_path):
-        if not os.path.exists(model_path):
-            raise FileNotFoundError(f"Model file not found: {model_path}")
-        self.model = self.load_model(model_path)
+class SingletonModelHandler:
+    _instance = None
+
+    def __new__(cls, model_path):
+        if cls._instance is None:
+            cls._instance = super(SingletonModelHandler, cls).__new__(cls)
+            cls._instance.model = cls._instance.load_model(model_path)
+        return cls._instance
 
     @staticmethod
     def load_model(model_path):
+        if not os.path.exists(model_path):
+            raise FileNotFoundError(f"Model file not found: {model_path}")
         return joblib.load(model_path)
 
     def predict(self, features):
